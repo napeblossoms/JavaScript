f5887d2d19c6917642f78e8f835df21a16ef15bd
napeblossoms <101855341+napeblossoms@users.noreply.github.com>
2022-07-12 22:31:51 +0900

    [Update]

--- 自主学習/self practice(s)/index.js
+++ 自主学習/self practice(s)/index.js
@@ -3,3 +3,8 @@ window.console.log();//水色＝プロパティ、黄色＝メソッド　window
 window.alert();
 
 window.document.getElementById(); //window.は省略可能
+
+const foo = document.getElementById("foo"); //変数宣言
+const fooWeight = foo.offsetWidth;
+const fooHeight = foo.offsetHeight; //長いコードの時は変数でおいて省略
+const fooArea = fooWeight * fooHeight
