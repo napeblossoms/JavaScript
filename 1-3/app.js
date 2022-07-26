20ba67c5d312895c33c6cb80e5de7c1f31eb544d
napeblossoms <101855341+napeblossoms@users.noreply.github.com>
2022-07-26 17:20:31 +0900

    1-3

---
+++ 1-3/app.js
@@ -0,0 +1,14 @@
+// let alertString;
+// alertString = addString("Webcamp");
+
+// //作成した関数を呼び出す
+// alert(alertString);
+
+// //作成した関数
+// function addString(strA) {
+//   let addStr = "Hello" + strA;
+//   return addStr;
+// }
+
+let promptStr = prompt('何か好きな文字入力してください');
+  alert(promptStr);
